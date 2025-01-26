093203e52fe59cfeaad5d5f430315becfa1e4151
ammad-ahmed1 <iammadmughal480@gmail.com>
2025-01-26 23:32:47 +0500

    project-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import Sidebar from "./components/shared/Sidebar";
 
 function App() {
-  return <div className="text-red-500">x-2.0</div>;
+  return (
+    <div className="text-red-500">
+      x-2.0
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/icons/SidebarIcons.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+export const HomeIcon = ({ size = 24, color = "#e7e9ea", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill={color}
+    {...props}>
+    <path d="M21.591 7.146L12.52 1.157c-.316-.21-.724-.21-1.04 0l-9.071 5.99c-.26.173-.409.456-.409.757v13.183c0 .502.418.913.929.913H9.14c.51 0 .929-.41.929-.913v-7.075h3.909v7.075c0 .502.417.913.928.913h6.165c.511 0 .929-.41.929-.913V7.904c0-.301-.158-.584-.408-.758z" />
+  </svg>
+);
+
+export const ExploreIcon = ({ size = 24, color = "#e7e9ea", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill={color}
+    {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14.5v-5l-4 2.5 4-2.5 4 2.5-4-2.5v5l4-2.5-4 2.5z" />
+  </svg>
+);
+
+export const NotificationsIcon = ({
+  size = 24,
+  color = "#e7e9ea",
+  ...props
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill={color}
+    {...props}>
+    <path d="M12 2a7 7 0 00-7 7v5.086l-1.293 1.293a1 1 0 000 1.414V18h16v-.207a1 1 0 000-1.414L19 14.086V9a7 7 0 00-7-7zm0 20a2 2 0 002-2H10a2 2 0 002 2z" />
+  </svg>
+);
+
+export const MessagesIcon = ({ size = 24, color = "#e7e9ea", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill={color}
+    {...props}>
+    <path d="M20 2H4a2 2 0 00-2 2v12a2 2 0 002 2h4l4 4 4-4h4a2 2 0 002-2V4a2 2 0 00-2-2zM6 9h12v2H6V9zm8 4H6v-2h8v2z" />
+  </svg>
+);
+
+export const CommunitiesIcon = ({ size = 24, color = "#e7e9ea", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill={color}
+    {...props}>
+    <path d="M12 4a4 4 0 11-4 4 4 4 0 014-4zm-6 6a2 2 0 10-2 2 2 2 0 002-2zm14 0a2 2 0 10-2 2 2 2 0 002-2zm-7 4c-3.333 0-8 1.667-8 5v1h16v-1c0-3.333-4.667-5-8-5z" />
+  </svg>
+);
+
+export const ProfileIcon = ({ size = 24, color = "#e7e9ea", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill={color}
+    {...props}>
+    <path d="M12 12a5 5 0 10-5-5 5 5 0 005 5zm0 2c-3.333 0-8 1.667-8 5v1h16v-1c0-3.333-4.667-5-8-5z" />
+  </svg>
+);
+
+export const SettingsIcon = ({ size = 24, color = "#e7e9ea", ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill={color}
+    {...props}>
+    <path d="M12 1a2 2 0 012 2v.806a7.972 7.972 0 013.134 1.81l.655-.655a2 2 0 112.828 2.828l-.655.655A7.972 7.972 0 0120.194 12H21a2 2 0 010 4h-.806a7.972 7.972 0 01-1.81 3.134l.655.655a2 2 0 01-2.828 2.828l-.655-.655A7.972 7.972 0 0112 20.194V21a2 2 0 01-4 0v-.806a7.972 7.972 0 01-3.134-1.81l-.655.655a2 2 0 01-2.828-2.828l.655-.655A7.972 7.972 0 013.806 12H3a2 2 0 010-4h.806a7.972 7.972 0 011.81-3.134l-.655-.655a2 2 0 012.828-2.828l.655.655A7.972 7.972 0 0112 3.806V3a2 2 0 014-2zM12 8a4 4 0 104 4 4 4 0 00-4-4z" />
+  </svg>
+);
