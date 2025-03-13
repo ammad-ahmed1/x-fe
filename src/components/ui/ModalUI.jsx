2f07b5f09d1fcd1a197d9347a1c426f0980184b5
Ammad Ahmed <63580683+ammad-ahmed1@users.noreply.github.com>
2025-03-13 05:26:05 +0500

    modal

--- src/components/ui/ModalUI.jsx
+++ src/components/ui/ModalUI.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { X } from "lucide-react";
 
 const ModalUI = () => {
-  return <div>ModalUI</div>;
+  return (
+    <div className="w-screen h-screen fixed inset-0  bg-black/50 z-50 flex justify-center items-center  ">
+      <div className=" w-[450px] h-[375px] bg-black rounded-2xl">
+        <div className="header flex p-4">
+          <X className="w-6 h-6 text-gray-700 text-white" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ModalUI;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,14 +4,24 @@ import { posts, profilesList, whatsHappening } from "../data/users-data";
 import MainContent from "../components/modules/home/MainContent";
 import RightSiderContent from "../components/modules/home/RightSiderContent";
 import HeaderContent from "../components/modules/home/HeaderContent";
+import ModalUI from "../components/ui/ModalUI";
 
 const Homepage = () => {
+  useState();
+  const [isShowModal, setIsShowModal] = useState(false);
+  const handleShowModal = () => {
+    setIsShowModal(!isShowModal);
+  };
   return (
     <>
+      <button onClick={handleShowModal}> Click me</button>
       <Layout
         headerContent={<HeaderContent />}
-        rightSidebarContent={<RightSiderContent />}>
+        rightSidebarContent={<RightSiderContent />}
+      >
         <MainContent posts={posts} />
+        <button>Click me</button>
+        {isShowModal && <ModalUI />}
       </Layout>
     </>
   );
