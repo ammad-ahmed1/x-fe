a559575c6ecbdd319f21da82afafc6e394afe051
ammad-ahmed1 <iammadmughal480@gmail.com>
2025-05-08 02:35:14 +0500

    latest

--- src/components/ui/TestModalComponent.jsx
+++ src/components/ui/TestModalComponent.jsx
@@ -3,11 +3,7 @@ import ButtonUI from "./ButtonUI";
 import TextFieldUI from "./TextFieldUI";
 import DatePickerUI from "./DatePickerUI";
 import useForm from "../../hooks/useForm";
-<<<<<<< Updated upstream
 import { signupErrorMessages, signupSchema } from "../../utils/validators";
-=======
-import { signupSchema } from "../../utils/validators";
->>>>>>> Stashed changes
 
 const TestModalComponent = () => {
   const [formData, setFormData] = useState({
@@ -32,7 +28,6 @@ const TestModalComponent = () => {
     provider: "google",
   };
 
-<<<<<<< Updated upstream
   const {
     values,
     errors,
@@ -52,18 +47,6 @@ const TestModalComponent = () => {
   });
   console.log(errors, ".......err");
   // console.log(values, ".......vals");
-=======
-  const { values, errors, handleFieldChange, handleSubmit, handleReset } =
-    useForm({
-      initialValues: formData,
-      errors: formErrors,
-      validationSchema: signupSchema,
-      onSubmit: (values) => {
-        console.log(values);
-      },
-    });
-
->>>>>>> Stashed changes
   return (
     <>
       <form
@@ -117,7 +100,6 @@ const TestModalComponent = () => {
             state={formData}
             fieldState={values?.user_name}
             onChange={handleFieldChange}
-            validation={signupSchema?.user_name}
             errorMsg={errors?.user_name}
             // validation={signupSchema?.user_email}
             disabled={false}
