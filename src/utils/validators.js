a7a2cabe230b900edf7dd696df46cd814836e128
ammad-ahmed1 <iammadmughal480@gmail.com>
2025-05-08 02:40:03 +0500

    fixes

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -14,28 +14,10 @@ export const regexValidators = {
     "yyyy/mm/dd": /^\d{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])$/,
   },
 };
-<<<<<<< Updated upstream
-
-export const signupSchema = {
-  user_email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-  user_password:
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-  user_name: /^[a-zA-Z]+([ '-][a-zA-Z]+)*$/,
-  user_dob: /^(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])\/\d{4}$/, // MM-DD-YYYY
-};
-export const signupErrorMessages = {
-  user_email: "Please enter a valid email address (e.g., user@example.com).",
-  user_password:
-    "Password must be at least 8 characters long, include one uppercase letter, one lowercase letter, one number, and one special character.",
-  user_name:
-    "Name should only contain letters and may include spaces, hyphens, or apostrophes.",
-  user_dob: "Please enter a valid date of birth in MM-DD-YYYY format.",
-=======
 export const signupSchema = {
   user_name: /^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]{3,16}(?<![_.])$/,
   user_email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
   user_password:
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
   user_dob: /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/, // YYYY-MM-DD
->>>>>>> Stashed changes
 };
