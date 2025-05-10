cdb96b5d2355f3dbb27aa52e4ee5c085e98363b4
ammad-ahmed1 <iammadmughal480@gmail.com>
2025-05-11 04:31:13 +0500

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-// import Layout from "./components/shared/Layout";
-import Homepage from "./pages/Homepage";
-import SignIn from "./pages/SignIn";
-import EmailSignUpForm from "./components/modules/auth/EmailSignUpForm";
-import ModalUI from "./components/ui/ModalUI";
-import TestModalComponent from "./components/ui/TestModalComponent";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
-  const [isSignupModalOpen, setIsSignUpModalOpen] = useState(true);
-  const [isSigninModalOpen, setIsSigninModalOpen] = useState(true);
   return (
     <div className="">
-      {/* <Homepage /> */}
-      <SignIn />
-      {/* <TestModalComponent /> */}
+      <AppRouter />
     </div>
   );
 }
